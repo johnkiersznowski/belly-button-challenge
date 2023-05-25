b50582ef2b10d04625815ef92ff037060a031b41
johnkiersznowski <johnkiersznowski@gmail.com>
2023-05-25 10:08:05 -0400

    commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,28 +1,30 @@
-// Load the D3 library
-d3.select("body").selectAll("script").remove();
-d3.import("https://d3js.org/d3.v5.min.js");
+// constant URL
+const DATA_URL = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
 
-// Load the samples data
-var samplesData = d3.json("https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json");
+// // Load the samples data
+// var samplesData = d3.json();
 
-// Create a dropdown menu to select a sample
-var selDataset = d3.select("#selDataset");
-selDataset.selectAll("option").remove();
-samplesData.forEach(function(sample) {
-  selDataset.append("option").text(sample.sample_id).value(sample.sample_id);
-});
+// // Create a dropdown menu to select a sample
+// var selDataset = d3.select("#selDataset");
+// selDataset.selectAll("option").remove();
+// samplesData.forEach(function(sample) {
+//   selDataset.append("option").text(sample.sample_id).value(sample.sample_id);
+// });
 
 // Create a function to update the charts when a new sample is selected
-function optionChanged(sampleId) {
+function updateDisplay(data, subjectID) {
   // Get the selected sample
-  var sample = samplesData.find(function(sample) {
-    return sample.sample_id === sampleId;
-  });
-
+//   var sample = data.metadata.filter(sampleID =>  {
+//     // return sample.sample_id === sampleId;
+//     sampleID.id === subjectID
+//   });
+// console.log(sample)
+var sample = data.samples.find(item => item.id == subjectID);
+console.log(sample)
   // Update the bar chart
   var barChart = d3.select("#bar");
   barChart.selectAll(".bar").remove();
-  barChart.selectAll(".bar").data(sample.top_10_otu_values).enter().append("div")
+  barChart.selectAll(".bar").data(sample.otu_ids).enter().append("div")
     .attr("class", "bar")
     .attr("width", function(d) {
       return d;
@@ -34,7 +36,7 @@ function optionChanged(sampleId) {
   // Update the bubble chart
   var bubbleChart = d3.select("#bubble");
   bubbleChart.selectAll(".bubble").remove();
-  bubbleChart.selectAll(".bubble").data(sample.otu_values).enter().append("circle")
+  bubbleChart.selectAll(".bubble").data(sample.otu_labels).enter().append("circle")
     .attr("r", function(d) {
       return d;
     })
@@ -68,5 +70,41 @@ function optionChanged(sampleId) {
     });
 }
 
-// Set the default sample
-optionChanged(samplesData[0].sample_id);
+// optionChanged
+function optionChanged(subjectID) {
+  d3.json(DATA_URL).then(data => updateDisplay(data, subjectID));
+}
+
+
+// Create a function to initialize the dashboard
+function init() {
+  // Use D3 to read samples.json from the URL
+  d3.json(DATA_URL).then(data => {
+      // Get the dropdown select element
+      const dropdown = d3.select("#selDataset");
+
+      // Get the sample names from the data
+      const sampleNames = data.names;
+
+      // Populate the dropdown with sample names
+      sampleNames.forEach(sample => {
+        dropdown
+          .append("option")
+          .text(sample)
+          .property("value", sample);
+      });
+
+      // Get the first sample from the data
+      const defaultSample = sampleNames[0];
+
+      // Update all plots with the default sample
+      optionChanged(defaultSample);
+    })
+    .catch(error => {
+      console.log("Error loading data:", error);
+    });
+}
+
+
+// 
+init();
